d881b6e96e687de1f1979d79e59abfc8d390b0a1
aliensmart <kaoua17@gmail.com>
2020-01-09 02:50:45 -0500

    add the route in the urls

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /pools/
     path('', views.index, name='index')
+     # ex: /pools/5/
+    path('/<int:question_id>/', views.index, name='detail')
+    # ex: /pools/5/result/
+    path('/<int:question_id>/result/', views.index, name='result')
+    # ex: /pools/5/vote/
+    path('/<int:question_id>/vote/', views.index, name='vote')
+    
 ]
