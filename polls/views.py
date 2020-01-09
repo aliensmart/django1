65d7b84c70087d58e2eb39e5b0c2cec56896af18
aliensmart <kaoua17@gmail.com>
2020-01-09 15:05:04 -0500

    form added in polls/detail.html

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 # from django.template import loader
-from .models import Question
+from .models import Question, Choice
 
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -36,5 +36,18 @@ def results(request, question_id):
     return HttpResponse(response.format(question_id))
 
 def vote(request, question_id):
-    response = "You're voting on question {}"
-    return HttpResponse(response.format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
+            'question':question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.vote +=1
+        selected_choice.save()
+        #Always return HttpResponseRedirect after successfully dealing
+        #with POST data. This prevents data from being posted twice if 
+        # user hists the Back Button
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
